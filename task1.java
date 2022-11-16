7db3dbd07c47d8c0ace08892145c778c06ceb967
Diagnoz_Aleshka <alekseypsih18@gmail.com>
2022-11-16 16:28:16 +0500

    Initial commit

---
+++ task1.java
@@ -0,0 +1,16 @@
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+    int res = 0;
+    int b = 5;
+    for(int i =1; i<=b;i++)
+    {
+        res+=i;
+    }
+    System.out.println(res);
+
+    }
+}
